315d3f8243d96345b55bfe491196018fe9eb4d0c
Ilya Rodionov <ris58h@gmail.com>
2018-10-01 21:47:24 +0300

    Node.

---
+++ node/index.js
@@ -0,0 +1,30 @@
+const http = require('http')
+
+const port = 8080
+
+http.createServer(function (req, res) {
+    const n = parseInt(req.url.substring(1))
+    if (n < 2) {
+        res.write('' + n)
+        res.end()
+    } else {
+        Promise.all([
+            fibonacci(n - 1),
+            fibonacci(n - 2)
+        ]).then(function (ns) {
+            const fibN = ns[0] + ns[1]
+            res.write('' + fibN)
+            res.end()
+        })
+    }
+}).listen(port)
+
+function fibonacci(n) {
+    return new Promise(function (resolve, reject) {
+        const request = http.get(`http://localhost:${port}/${n}`, function (res) {
+            let rawData = ''
+            res.on('data', (chunk) => { rawData += chunk })
+            res.on('end', () => { resolve(parseInt(rawData)) })
+        })
+    })
+}
